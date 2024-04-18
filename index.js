cae426a164843d35c6b298af0949b3b60a03b01f
RusselBRex-2002 <82494302+RusselBRex-2002@users.noreply.github.com>
2024-04-18 12:18:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,23 +5,45 @@ const nodemailer = require('nodemailer');
 const workbook = xlsx.readFile('data.xlsx');
 const worksheet = workbook.Sheets['Sheet1'];
 
-const columnToExtract = 'A';
+const f = 'F';
+const s = 'S';
+const y = 'Y';
+const as = 'AS';
+
+
 
 const columnData = [];
+const technical_service = []
+const instance_name = []
+const instance_account = []
+const expiry_date = []
+
 let rowIndex = 1;
 
 const intervalset = setInterval(() => {
-    if (worksheet[`${columnToExtract}${rowIndex}`] !== undefined) {
-        columnData.push(worksheet[`${columnToExtract}${rowIndex}`].v);
-        sendEmail(columnData[columnData.length-1])
+    if (worksheet[`${y}${rowIndex}`] !== undefined) {
+        instance_account.push(worksheet[`${y}${rowIndex}`].v);
+        technical_service.push(worksheet[`${f}${rowIndex}`].v);
+        instance_name.push(worksheet[`${s}${rowIndex}`].v);
+        expiry_date.push(worksheet[`${as}${rowIndex}`].w);
+        const lastIndex = expiry_date.length - 1
+        const daysLeft = 90
+        const expiryDate = new Date(expiry_date[lastIndex]);
+        const currentDate = new Date();
+        const differenceInDays = Math.floor((currentDate - expiryDate) / (1000 * 60 * 60 * 24));
+        if (differenceInDays <= daysLeft) {
+            console.log(`${instance_name[lastIndex]}, your password is due for ${daysLeft}. Other details, technical service: ${technical_service[lastIndex]}, instance account: ${instance_account[lastIndex]}
+            `);
+        } 
+        // sendEmail(columnData[columnData.length-1])
         rowIndex++;
     }else{
         clearInterval(intervalset)
     }    
 
-}, 5000);
+}, 2000);
 
-const sender_id = 'abc@xyz.com';
+// const sender_id = 'abc@xyz.com';
 
 function sendEmail(receiver) {
 
@@ -65,7 +87,3 @@ function sendEmail(receiver) {
 
     
 }
-
-
-
-
