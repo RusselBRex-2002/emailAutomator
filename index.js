e941c77b030181449cdb9392f6c95a0ad06ca7c8
RusselBRex-2002 <82494302+RusselBRex-2002@users.noreply.github.com>
2024-04-18 12:24:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -35,55 +35,9 @@ const intervalset = setInterval(() => {
             console.log(`${instance_name[lastIndex]}, your password is due for ${daysLeft}. Other details, technical service: ${technical_service[lastIndex]}, instance account: ${instance_account[lastIndex]}
             `);
         } 
-        // sendEmail(columnData[columnData.length-1])
         rowIndex++;
     }else{
         clearInterval(intervalset)
     }    
 
 }, 2000);
-
-// const sender_id = 'abc@xyz.com';
-
-function sendEmail(receiver) {
-
-    const transporter = nodemailer.createTransport({
-        service: 'Outlook',
-        auth: {
-            user: sender_id,
-            pass: 'XXXXXXXX'
-        }
-    });
-
-    const missedIds = []
-
-    fs.readFile('emailContent.txt', 'utf8', function(err, data) {
-        if (err) {
-            console.error('Error reading file:', err);
-            return;
-        }
-    
-        // Assign the content of the text file to the 'text' property of 'mailOptions'
-        const mailOptions = {
-            from: sender_id,
-            to: receiver,
-            subject: 'Password expired',
-            text: data 
-        };
-    
-        transporter.sendMail(mailOptions, function (error, info) {
-            if (error) {
-                console.log(error);
-                missedIds.push(receiver);
-                console.log("Missed Ids: " + missedIds);
-            } else {
-                console.log('Email sent: ' + info.response);
-                console.log(receiver);
-            }
-    
-            transporter.close();
-        });
-    });
-
-    
-}
